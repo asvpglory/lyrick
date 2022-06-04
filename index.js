b4ddc9dc7d4f920d45555289305aeaf21735cecc
Glory <gloryomosomwan@gmail.com>
2022-06-04 16:47:12 -0600

    Added 'dotenv' package. Completed getArtistAlbums(), chooseAlbumId(), and chooseTrackId().

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { getArtistAlbums, chooseAlbum } = require('./modules');
+const { getArtistAlbums, chooseAlbumId, chooseTrackId } = require('./modules');
 
 async function lyrick() {
-    const albumList = await getArtistAlbums(118);
-    console.log(albumList);
+    const albumList = await getArtistAlbums(13928405);
+    const albumId = chooseAlbumId(albumList);
+    const trackList = await chooseTrackId(albumId);
 }
 
 lyrick();
-
--- modules.js
+++ modules.js
@@ -1,12 +1,22 @@
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
 
 module.exports.getArtistAlbums = async (artistId) => {
-    const apiKey = process.env.API_KEY;
     const res = await fetch(`https://api.musixmatch.com/ws/1.1/artist.albums.get?apikey=${apiKey}&artist_id=${artistId}&g_album_name=1`);
     const data = await res.json();
     const albumList = data.message.body.album_list;
     return albumList;
 };
 
-module.exports.chooseAlbum = async (albumList) => {
-    console.log(albumList);
+module.exports.chooseAlbumId = (albumList) => {
+    const albumIds = albumList.map(albumObject => albumObject.album.album_id);
+    const albumId = albumIds[Math.floor(Math.random() * albumIds.length)];
+    return albumId;
+};
+
+module.exports.chooseTrackId = async (albumId) => {
+    const res = await fetch(`https://api.musixmatch.com/ws/1.1/album.tracks.get?apikey=${apiKey}&album_id=${albumId}`);
+    const data = await res.json();
+    const trackList = data.message.body.track_list;
+    return trackList;
 };
