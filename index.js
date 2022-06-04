6474a5f0475bf5733e8aec572bd354c2b06273a1
Glory <gloryomosomwan@gmail.com>
2022-06-04 17:45:33 -0600

    Trying different loops.

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const { getArtistAlbums, chooseAlbum, getTrackList, chooseTrack, getSnippet, verifySnippet } = require('./modules');
 
 async function lyrick() {
-    setInterval(async () => {
+    // setInterval(async () => {
+    //     const albumList = await getArtistAlbums(13928405);
+    //     const albumId = chooseAlbum(albumList);
+    //     const trackList = await getTrackList(albumId);
+    //     const trackId = chooseTrack(trackList);
+    //     const snippet = await getSnippet(trackId);
+    //     if (verifySnippet(snippet)) {
+    //         console.log(snippet);
+    //     }
+    // }, 2000);
+    while (true) {
         const albumList = await getArtistAlbums(13928405);
         const albumId = chooseAlbum(albumList);
         const trackList = await getTrackList(albumId);
         const trackId = chooseTrack(trackList);
         const snippet = await getSnippet(trackId);
+
         if (verifySnippet(snippet)) {
-            console.log(snippet);
+            setTimeout(async () => {
+                await console.log(snippet);
+            }, 3000);
         }
-    }, 2000);
+    }
 }
 
 lyrick();
